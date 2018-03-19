3059ae0115c1f0cade50f0da6ddb4a464f0d3784
RONALD PIACENTI JUNIOT <rpiacentijr@yahoo.com.br>
2018-03-19 12:40:32 -0300

    Initial commit

---
+++ dump_produtos.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM casadocodigo_nodejs.produtos
+LIMIT 0, 50000
+
+-- Date: 2018-03-19 11:33
+*/
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (1,'Comecando com nodejs','livro introdutório sobre nodejs',39.90);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (2,'Comecando com javascript','livro introdutório sobre javascript',39.90);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (3,'Comecando com express','livro introdutório sobre express',39.90);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (4,'ABC','O Livro',150.00);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (5,'ABCNEWS BOOK','O Livro 2',250.00);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (6,'mais sobre node','node, javascript e um pouco sobre http',100.00);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (7,'adds','dasdadaddsas',145.00);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (8,'mais sobre node','node, javascript e um pouco sobre http',100.00);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (9,'titulo','novo livro',20.50);
+INSERT INTO `produtos` (`id`,`titulo`,`descricao`,`preco`) VALUES (10,'titulo','novo livro',20.50);
